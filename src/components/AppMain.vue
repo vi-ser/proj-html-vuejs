9ac187d60846fcd8ce51169edd298c84df69abc7
vi_ser <vinceulisse@gmail.com>
2024-03-11 16:29:19 +0100

    stilizzazione offerte

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,17 @@
 <script>
+import OffersItem from '../components/OffersItem.vue'
 export default {
     name: 'AppMain',
+
+    components: {
+        OffersItem,
+    }
 
 }
 </script>
 
 <template>
-
+    <OffersItem></OffersItem>
 </template>
 
 <style lang="scss"></style>
---
+++ src/components/OffersItem.vue
@@ -0,0 +1,97 @@
+<script>
+export default {
+    name: 'OffersItem',
+}
+</script>
+
+<template>
+    <div class="offers d-flex">
+        <div class="left d-flex justify-content-center align-items-center">
+            <img src="/img/h1-img-7n.png" alt="">
+        </div>
+        <div class="right">
+            <h3 class="text-uppercase oswald-medium">
+                specials*
+            </h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesent ut!</p>
+            <div class="menu d-flex">
+                <span class="price oswald-medium">$10</span>
+                <div class="offer-item">
+                    <span class="offer text-uppercase oswald-medium">combo
+                        piccolo</span>
+                    <p class="item-descr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesent ut aliquam
+                        dui</p>
+                </div>
+            </div>
+            <div class="menu d-flex">
+                <span class="price oswald-medium">$20</span>
+                <div class="offer-item">
+                    <span class="offer text-uppercase oswald-medium">combo
+                        medio</span>
+                    <p class="item-descr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesent ut aliquam
+                        dui</p>
+                </div>
+            </div>
+            <div class="menu d-flex">
+                <span class="price oswald-medium">$30</span>
+                <div class="offer-item">
+                    <span class="offer text-uppercase oswald-medium">combo
+                        grande</span>
+                    <p class="item-descr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesent ut aliquam
+                        dui</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general' as *;
+@use '../styles/_variables' as *;
+
+.offers {
+
+    .left {
+        width: 50%;
+        background-image: url('/img/h1-img-4.jpg');
+        background-size: cover;
+        background-position: center;
+
+        img {
+            width: 370px;
+        }
+    }
+
+    .right {
+        width: 50%;
+        background-color: white;
+        padding: 155px 335px 195px 230px;
+        color: $text;
+
+        h3 {
+            font-size: 2em;
+            margin-bottom: 25px;
+            padding: 0;
+        }
+
+        p {
+            margin-bottom: 35px;
+            color: $textLight;
+        }
+
+        .price {
+            color: $primaryDark;
+            margin-right: 10px;
+            margin-bottom: 15px;
+        }
+
+        .offer-item {
+            .item-descr {
+                font-size: .8em;
+            }
+        }
+    }
+
+
+}
+</style>
