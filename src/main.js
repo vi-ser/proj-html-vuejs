fa97b96ebc1be35676ca7c6c621e8ae8f80b671f
vi_ser <vinceulisse@gmail.com>
2024-03-11 11:50:17 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './styles/general.scss'
+import "bootstrap/dist/css/bootstrap.css";
+
+
+createApp(App).mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js";
