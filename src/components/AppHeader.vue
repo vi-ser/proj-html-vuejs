a8145bb02e79f74fb452228bd65caac23d3bad85
vi_ser <vinceulisse@gmail.com>
2024-03-11 14:45:56 +0100

    carosello jumbo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,14 @@
 <script>
 
 import NavItem from '../components/NavItem.vue'
+import JumboCarousel from '../components/JumboCarousel.vue'
+
 export default {
     name: 'AppHeader',
 
     components: {
         NavItem,
+        JumboCarousel,
     },
 }
 </script>
@@ -18,18 +21,21 @@ export default {
             <img class="jumbo-img" src="/img/h3-rev-img-6.png" alt="">
             <img class="text-bg" src="/img/h3-rev-img-5.png" alt="">
         </div>
-
+        <JumboCarousel>
+        </JumboCarousel>
     </div>
 </template>
 
 <style lang="scss" scoped>
 .header {
     background-image: url('/img/cielostellato.PNG');
-    height: 610px;
+    /* height: 610px; */
 
     .img-container {
         height: 400px;
         position: relative;
+        margin-bottom: 60px;
+
 
         .jumbo-img {
             height: 100%;
---
+++ src/components/JumboCarousel.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: 'JumboCarousel',
+
+}
+</script>
+
+<template>
+    <div id="carousel" class="d-flex">
+        <img src="/img/h3-img-1.jpg" alt="">
+        <img src="/img/h3-img-2.jpg" alt="">
+        <img src="/img/h3-img-3.jpg" alt="">
+        <img src="/img/h3-img-4.jpg" alt="">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general' as *;
+@use '../styles/_variables' as *;
+
+#carousel {
+    padding: $smallMargin 0;
+    gap: $smallMargin;
+
+    background-color: white;
+
+    img {
+        width: calc(100% / 4 - $smallMargin / 4 * 3);
+        height: 100%;
+    }
+}
+</style>
