c53c71bc6c1fff6d7c698329619051894c362a96
vi_ser <vinceulisse@gmail.com>
2024-03-13 12:02:48 +0100

    aggiunto hover alle immagini del jumbo

--- src/components/JumboCarousel.vue
+++ src/components/JumboCarousel.vue
@@ -7,10 +7,38 @@ export default {
 
 <template>
     <div id="carousel" class="d-flex">
-        <img src="/img/h3-img-1.jpg" alt="">
-        <img src="/img/h3-img-2.jpg" alt="">
-        <img src="/img/h3-img-3.jpg" alt="">
-        <img src="/img/h3-img-4.jpg" alt="">
+        <div class="image-card">
+            <div class="front-card">
+                <img src="/img/h3-img-1.jpg" alt="">
+            </div>
+            <div class="back-card">
+                <i class="fa-regular fa-eye"></i>
+            </div>
+        </div>
+        <div class="image-card">
+            <div class="front-card">
+                <img src="/img/h3-img-2.jpg" alt="">
+            </div>
+            <div class="back-card">
+                <i class="fa-regular fa-eye"></i>
+            </div>
+        </div>
+        <div class="image-card">
+            <div class="front-card">
+                <img src="/img/h3-img-3.jpg" alt="">
+            </div>
+            <div class="back-card">
+                <i class="fa-regular fa-eye"></i>
+            </div>
+        </div>
+        <div class="image-card">
+            <div class="front-card">
+                <img src="/img/h3-img-4.jpg" alt="">
+            </div>
+            <div class="back-card">
+                <i class="fa-regular fa-eye"></i>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -24,9 +52,44 @@ export default {
 
     background-color: white;
 
-    img {
+    .image-card {
+        position: relative;
         width: calc(100% / 4 - $smallMargin / 4 * 3);
-        height: 100%;
+
+
+
+        img {
+            width: 100%;
+        }
+
+        .back-card {
+            position: absolute;
+            top: 0;
+            left: 0;
+
+            display: none;
+            justify-content: center;
+            align-items: center;
+
+            width: 100%;
+            height: 100%;
+
+            background-image: url('/img/pattern.png');
+
+
+            i {
+                color: white;
+                background-color: $primary;
+                padding: 20px;
+                border-radius: 50%;
+                cursor: pointer;
+            }
+
+        }
+
+        &:hover .back-card {
+            display: flex;
+        }
     }
 }
 </style>
